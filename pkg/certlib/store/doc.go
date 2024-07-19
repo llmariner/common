9960616e53c65239a675a7951a17970598b3bde6
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-07-19 09:40:05 -0700

    Port certlib from cloudnatix
    This will be used by session-manager and inference-manager-server
    to use TLS in their servers. As these servers provide the endpoint
    without Kong, they need to handle TLS requests.

---
+++ pkg/certlib/store/doc.go
@@ -0,0 +1,4 @@
+// Package store provides various implementations of stores of TLS certificates
+// that can be plugged into server libraries that accept tls.Config structs
+// (e.g. the http and grpc packages).
+package store
